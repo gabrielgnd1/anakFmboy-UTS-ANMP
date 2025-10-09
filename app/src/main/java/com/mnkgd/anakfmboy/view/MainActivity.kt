3041ba7bb819c3753941444decf4b53c3a756f1c
Gabriel Narendra <gabrielnarendra1@gmail.com>
2025-10-09 18:08:37 +0700

    fixed minor navController di home, tolong fix constraint donk

--- app/src/main/java/com/mnkgd/anakfmboy/view/MainActivity.kt
+++ app/src/main/java/com/mnkgd/anakfmboy/view/MainActivity.kt
@@ -5,16 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.mnkgd.anakfmboy.R
 import com.mnkgd.anakfmboy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+
+
+        navController = (supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment).navController
+        NavigationUI.setupWithNavController(binding.bottomNav, navController)
+
+        binding.bottomNav.setupWithNavController(navController)
 
     }
 }
