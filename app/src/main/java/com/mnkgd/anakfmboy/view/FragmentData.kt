7137e92b773f5d79cc6ab0ccac8b18778562901e
hyperneutr0n <randylemuel21@gmail.com>
2025-10-21 03:21:04 +0700

    fragment data done

--- app/src/main/java/com/mnkgd/anakfmboy/view/FragmentData.kt
+++ app/src/main/java/com/mnkgd/anakfmboy/view/FragmentData.kt
@@ -5,15 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
-import com.mnkgd.anakfmboy.R
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mnkgd.anakfmboy.databinding.FragmentDataBinding
+import com.mnkgd.anakfmboy.viewmodel.UkurViewModel
 
 class FragmentData : Fragment() {
     private lateinit var binding: FragmentDataBinding
     private lateinit var navController: NavController
-
-
+    private lateinit var viewModel: UkurViewModel
+    private val ukurListAdapter = UkurListAdapter(arrayListOf())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,7 +31,18 @@ class FragmentData : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProvider(this)[UkurViewModel::class.java]
+        viewModel.refresh()
+
+        binding.recData.layoutManager = LinearLayoutManager(context)
+        binding.recData.adapter = ukurListAdapter
+
+        observeViewModel()
     }
 
-
+    fun observeViewModel() {
+        viewModel.ukurLD.observe(viewLifecycleOwner, Observer {
+            ukurListAdapter.updateUkurList(it)
+        })
+    }
 }
---
+++ app/src/main/java/com/mnkgd/anakfmboy/view/UkurListAdapter.kt
@@ -0,0 +1,56 @@
+package com.mnkgd.anakfmboy.view
+
+import android.graphics.Typeface
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.mnkgd.anakfmboy.databinding.DataUkurItemBinding
+import com.mnkgd.anakfmboy.model.Ukur
+
+class UkurListAdapter(val ukurList: ArrayList<Ukur>): RecyclerView.Adapter<UkurListAdapter.DataUkurViewHolder>() {
+    class DataUkurViewHolder (var binding: DataUkurItemBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): DataUkurViewHolder {
+        val binding = DataUkurItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return DataUkurViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(
+        holder: DataUkurViewHolder,
+        position: Int
+    ) {
+        if (position == 0) {
+            // --- THIS IS THE HEADER (Position 0) ---
+            holder.binding.txtAge.text = "Age"
+            holder.binding.txtHeight.text = "Height (cm)"
+            holder.binding.txtWeight.text = "Weight (kg)"
+
+            // Set text style to BOLD
+            holder.binding.txtAge.setTypeface(null, Typeface.BOLD)
+            holder.binding.txtHeight.setTypeface(null, Typeface.BOLD)
+            holder.binding.txtWeight.setTypeface(null, Typeface.BOLD)
+
+        } else {
+            val dataPosition = position - 1
+
+            holder.binding.txtAge.text = ukurList[dataPosition].usia.toString()
+            holder.binding.txtHeight.text = ukurList[dataPosition].tinggi.toString()
+            holder.binding.txtWeight.text = ukurList[dataPosition].berat.toString()
+
+            holder.binding.txtAge.setTypeface(null, Typeface.NORMAL)
+            holder.binding.txtHeight.setTypeface(null, Typeface.NORMAL)
+            holder.binding.txtWeight.setTypeface(null, Typeface.NORMAL)
+        }
+    }
+
+    override fun getItemCount(): Int = ukurList.size + 1
+
+    fun updateUkurList(newUkurList: ArrayList<Ukur>) {
+        ukurList.clear()
+        ukurList.addAll(newUkurList)
+        notifyDataSetChanged()
+    }
+}
